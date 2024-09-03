d16b9b8675aeb4b7ada9dbb1096d2cd777209210
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-09-03 12:06:35 +0900

    [Bronze I] Title: 주디와 당근농장, Time: 276 ms, Memory: 18284 KB -BaekjoonHub

--- 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.java
+++ 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.java
@@ -4,32 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        String [][] list = new String[num1][num1];
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        if ((num2+num3) % 2 == 0){
-            for (int i = 0; i < num1; i++) {
-                for (int j = 0; j < num1; j++) {
-                    if ((i + j) % 2 == 0) {
-                        list[i][j] = "v";
-                    } else list[i][j] = ".";
-                }
-            }
-        }else{
-            for (int i = 0; i < num1; i++) {
-                for (int j = 0; j < num1; j++) {
-                    if ((i+j)% 2 != 0) {
-                        list[i][j] = "v";
-                    }else list[i][j] = ".";
-                }
-            }
-        }
-
-        for (int i=0; i<num1;i++){
-            for (int n=0; n<num1;n++){
-                System.out.printf("%s",list[i][n]);
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if ((b+c) % 2 == (i + j) % 2) {
+                    System.out.print("v");
+                } else System.out.print(".");
             }
             System.out.println();
         }
