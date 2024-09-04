828bd41cc2910a0dd1abed3fd69b3775e19e04fe
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-09-04 21:39:17 +0900

    Update 약수.java

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -5,13 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a= sc.nextInt();
-        sc.nextLine();
-        String[] list = sc.nextLine().split(" ");
         int[] nums = new int[a];
-        for (int i=0;i<a;i++) {
-            nums[i] = Integer.parseInt(list[i]);
-        }
+        for (int i=0;i<a;i++) nums[i] = sc.nextInt();
         Arrays.sort(nums);
-        System.out.println(nums[0] * nums[list.length-1]);
+        System.out.println(nums[0] * nums[a-1]);
         }
     }
