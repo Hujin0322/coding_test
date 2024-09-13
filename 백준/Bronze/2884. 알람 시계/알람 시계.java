26788169c368a528eb04148a656e2db254f05fa4
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-09-13 13:04:13 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -5,11 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         int m = sc.nextInt();
-        if (m<45){
-            if(h==0) h=23;
-            else{h=h-1;}
-            m=m+60-45;
-        }else{m=m-45;}
-        System.out.print(h+" "+m);
+        System.out.print(m<45?(h+23)%24+" "+(m+15):h+" "+(m-45));
     }
 }
