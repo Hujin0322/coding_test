a1b1d0a71c6a465b7e0875eec4f88737d5acdb2b
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-09-20 02:03:40 +0900

    [Bronze I] Title: FizzBuzz, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,18 +1,9 @@
-list = []
-for i in range(3):
+for i in range(3,0,-1):
     a = input()
-    if a.isdigit():
-        list.append(int(a))
-    else: list.append(a)
-
-for i, n in enumerate(list):
-    if isinstance(n,int):
-        if i==0:next=n+3
-        elif i==1:next=n+2
-        else: next=n+1
+    if a not in ['Fizz','Buzz','FizzBuzz']:
+        next = int(a) + i
         
-if next%3==0:
-    if next%5==0:print("FizzBuzz")
-    else: print("Fizz")
-elif next%5==0:print("Buzz")
+if next%15==0:print('FizzBuzz')
+elif next%3==0:print('Fizz')
+elif next%5==0:print('Buzz')
 else: print(next)
