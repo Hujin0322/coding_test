18ac5392b12fda494d97c08fed0c8a6bf72e4392
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-09-20 03:05:07 +0900

    [Silver V] Title: 단어 정렬, Time: 520 ms, Memory: 36552 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,10 +1,8 @@
 n = int(input())
 list = []
 for i in range(n):
-    a= input()
+    a = input()
     list.append(a)
-list = set(list)
-result = sorted(list, key=lambda x:(len(x),x))
+result = sorted(set(list), key=lambda x:(len(x),x))
 
-for i in range(len(result)):
-    print(result[i])
+print("\n".join(result))
