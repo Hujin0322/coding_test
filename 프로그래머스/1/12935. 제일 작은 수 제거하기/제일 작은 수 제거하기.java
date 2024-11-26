50b1b0d085781e36e49ceecbde29104e66a1f74e
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-11-26 10:53:55 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.93 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return (arr[0] == 10)? new int[]{-1} : arr;
+        }
+
+        int min = arr[0];
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        int[] ans = new int[arr.length-1];
+        int i = 0;
+        for (int num : arr) {
+            if (num != min) {
+                ans[i++] = num;
+            }
+        }return ans;
+    }
+}
