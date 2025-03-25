1d555c953b8109b901ea7ce067225514ee8ef936
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2025-03-26 00:26:04 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 596 ms, Memory: 61372 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,26 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        long[] S = new long[N + 1];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
-        }
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
-        for (int a = 0; a < M; a++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            System.out.println(S[j] - S[i - 1]);
+        int[] S = new int[N+1];
+
+        String[] numbers = br.readLine().split(" ");
+        for (int i=1; i<=N;i++){
+            S[i] = S[i-1] + Integer.parseInt(numbers[i-1]);
         }
+        
+        for (int a=0;a<M;a++){
+            String[] input2 = br.readLine().split(" ");
+            int i = Integer.parseInt(input2[0]);
+            int j = Integer.parseInt(input2[1]);
+            bw.write((S[j]-S[i-1]) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
